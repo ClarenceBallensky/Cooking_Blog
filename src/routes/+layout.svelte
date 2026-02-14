fbe80d142672329c25d3a8d879248c490499bea3
Clarence Ballensky <alballensky@cpp.edu>
2026-02-13 20:23:36 -0800

    Working on the home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,4 +9,18 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+
+<header class="sticky top-0 z-50 border-b-2 bg-white/80 backdrop-blur">
+	<div class="mx-auto px-6">
+		<nav class="flex gap-4 py-4">
+			<h2> <a href="/">Favorite Dishes</a> </h2>
+			<h2><a href="/adventures">Cooking Adventures</a></h2>
+			<h2><a href="/about">About Me</a></h2>
+		</nav>
+	</div>
+</header>
+
+<main> 
+	{@render children()}
+</main>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,57 @@
+<script lang=ts>
+    import * as Select from "$lib/components/ui/select/index.js";
+    
+    const variations = [
+        { value: "gcheese", label: "Sandwhich"},
+        { value: "ques", label: "Loaded Quesadilla"}
+    ];
 
+    let value = $state("");
+
+    const triggerContent = $derived(
+        variations.find((f) => f.value === value)?.label ?? "Select your favorite"
+    );
+</script>
 
 <div class="p-6">
-    <h1 class="text-4xl font-bold mb-4">Clarence's Food Blog</h1>
-    <p>{count}</p>
-    <button type="button" class="bg-green-500 border-black border-2 rounded hover:bg-green-700" onclick={() => count++}>Increase Number</button>
-    <button type="button" class="bg-red-500 border-black border-2 rounded hover:bg-red-700" onclick={() => count--}>Decrease Number</button>
-    <p>Click <a class="text-blue-500 underline hover:text-blue-700" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1">here</a> for fun button facts!</p>
+   <h1 class="mb-6">Clarence's Cooking Blog</h1>
+
+    <div class="p-4 bg-white/80 rounded-lg">
+
+       <img src="/images/Delicious_Sandwich.jpg" 
+             alt="Delicious Sandwhich" 
+              class="w-64 rounded-lg float-left mr-6 mb-4" 
+        />
+
+        <p class="leading-relaxed wrap-break-words">
+            Whether you prefer a sandwhich or a quesadilla, the end result is the same: a complete meal containing all essential food groups. 
+            I start by frying up a simple quesadilla or grilled cheese in the pan. For quesadillas, I use a Mexican cheese blend, and for 
+            sandwhiches, I use swiss cheese. Optionally, fry an egg sunny-side up to add in later. 
+            Once the quasadilla/grilled cheese is crispy and overflowing with gooey cheese, I open it up and start stuffing it with extra 
+            ingredients. This begin recipe will not only satisfy your appetite; it will boost your confidence, allowing you to progress 
+            to more complex dishes. 
+        </p>
+
+        <Select.Root type="single" name="favoriteFruit" bind:value>
+            <Select.Trigger class="w-45 text-base font-inherit">
+                {triggerContent}
+            </Select.Trigger>
+            <Select.Content class="text-base font-inherit">
+                <Select.Group>
+                    <Select.Label>Variations</Select.Label>
+                    {#each variations as variation (variation.value)}
+                        <Select.Item
+                            value={variation.value}
+                            label={variation.label}
+                        >
+                            {variation.label}
+                        </Select.Item>
+                    {/each}
+                </Select.Group>
+            </Select.Content>
+        </Select.Root>
+   
+    </div>
 </div>
 
 
-<script lang=ts>
-    let count: number=$state(0);
-    let is4 = $derived(count == 4);
-    $effect(() => {
-        if (count === 100) {
-            alert("fireworks");
-        } 
-    })
-</script>
